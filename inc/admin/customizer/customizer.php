c86e0c1d002a25afc2107acd4156c74680cb09cb
romik84 <blogatize.net@gmail.com>
2018-07-27 07:29:53 +0200

    Small Updates

--- inc/admin/customizer/customizer.php
+++ inc/admin/customizer/customizer.php
@@ -22,18 +22,22 @@ class evolve_Customizer {
 
 	function __construct() {
 		add_action( 'customize_controls_enqueue_scripts', array( $this, 'custom_customize_enqueue' ), 9999 );
-		add_action( 'customize_controls_print_footer_scripts', array( $this, 'filter_ajax_url_for_customizer_live_preview' ), 9999 );
+		add_action( 'customize_controls_print_footer_scripts', array(
+			$this,
+			'filter_ajax_url_for_customizer_live_preview'
+		), 9999 );
 	}
-	public function filter_ajax_url_for_customizer_live_preview()
-	{
-		?>
-		<script type="text/javascript">
-		if(_wpCustomizeSettings.theme.active == false){
-			ajaxurl = '<?php echo admin_url('admin-ajax.php'); ?>?customize_changeset_uuid='+_wpCustomizeSettings.changeset.uuid+'&customize_theme='+_wpCustomizeSettings.theme.stylesheet;
-		}
-		</script>
-		<?php
-	}
+
+	public function filter_ajax_url_for_customizer_live_preview() { ?>
+
+        <script type="text/javascript">
+            if (_wpCustomizeSettings.theme.active == false) {
+                ajaxurl = '<?php echo admin_url( 'admin-ajax.php' ); ?>?customize_changeset_uuid=' + _wpCustomizeSettings.changeset.uuid + '&customize_theme=' + _wpCustomizeSettings.theme.stylesheet;
+            }
+
+        </script>
+	<?php }
+
 	public function custom_customize_enqueue() {
 		wp_enqueue_style( 'evolve-customizer-icon', get_template_directory_uri() . '/inc/admin/customizer/assets/fonts/fontastic/styles.min.css', array(), '', 'all' );
 		wp_enqueue_style( 'evolve-customizer-css', get_template_directory_uri() . '/inc/admin/customizer/assets/css/customizer.min.css', array(), '', 'all' );
